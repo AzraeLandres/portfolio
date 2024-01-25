65f69c16e60d78598ba8088f0a789cf893e7d2af
AzraeLandres <azraellandres@gmail.com>
2024-01-25 14:50:16 +0100

    path

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./dist/"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/portfolio" });
+  res.sendFile("index.html", { root: "dist/" });
 });
 
 app.listen(process.env.PORT || 8080);
