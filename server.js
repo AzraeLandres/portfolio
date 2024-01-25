c4a8fc6ba07aa3d623c9fdc9b738636e59757e9e
AzraeLandres <azraellandres@gmail.com>
2024-01-25 15:02:11 +0100

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static("./dist/"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("index.html", { root: "dist/portfolio/browser" });
+  res.sendFile("index.html", { root: "dist/portfolio/" });
 });
 
 app.listen(process.env.PORT || 8080);
