6efe62ac94dc4bcd6b103a38c705d2675f90e996
AzraeLandres <azraellandres@gmail.com>
2024-01-25 21:03:57 +0100

    ...

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const path = require("path");
 //Install express server
+
 const express = require("express");
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static(__dirname + "/dist/browser"));
 
-app.get((req, res) => {
-  res.sendFile("index.html", { root: "dist/browser" });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/browser/index.html"));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
