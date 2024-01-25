b941f8ce25427d5c0f7b55ee4babb5d0cd231d2c
AzraeLandres <azraellandres@gmail.com>
2024-01-25 14:47:22 +0100

    ...

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("./src/"));
+app.use(express.static("./dist/"));
 
 app.get("/*", (req, res) => {
-  res.sendFile("app.component.html", { root: "src/app/" });
+  res.sendFile("index.html", { root: "dist/portfolio" });
 });
 
 app.listen(process.env.PORT || 8080);
